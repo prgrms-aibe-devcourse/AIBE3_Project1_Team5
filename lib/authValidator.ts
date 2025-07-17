d9a027375e8fd6f2fa386c9a3f637407f5ca2b0b
ehgk4245 <ehgkrk14@naver.com>
2025-07-17 16:18:49 +0900

    [feat]authValidator를 통해 유효성검사, useInputValidator를 통해 authValidator를 사용 및 에러 메시지 상태 관리

---
+++ lib/authValidator.ts
@@ -0,0 +1,21 @@
+// 인증 관련 입력값 검증 유틸
+
+export function isValidEmail(email: string): boolean {
+  // 간단한 이메일 정규식
+  return /^[\w-.]+@[\w-]+\.[a-z]{2,}$/i.test(email);
+}
+
+export function isValidPassword(password: string): boolean {
+  // 6~20자, 소문자/숫자/특수문자만 허용
+  // 특수문자: !@#$%^&*()
+  return /^[a-z0-9!@#$%^&*()]{6,20}$/.test(password);
+}
+
+export function isValidName(name: string): boolean {
+  // 2~15자, 한글/영문/숫자/_ 만 허용
+  return /^[가-힣a-zA-Z0-9_]{2,15}$/.test(name);
+}
+
+export function isPasswordMatch(password: string, confirmPassword: string): boolean {
+  return password === confirmPassword;
+}
