4c180e6d394fa44facc5e4aa6b0b5e2605aded84
weare0 <lay3699@naver.com>
2025-07-20 16:30:27 +0900

    [Refactor] 타입 변경

--- utils/destination/types.ts
+++ utils/destination/types.ts
@@ -10,14 +10,16 @@ export interface Destination {
   cost_hotel_per_night?: number;
   cost_meal_per_day?: number;
   cost_sightseeing_per_day?: number;
+  cost_etc_per_day?: number; 
+  detail_description?: string;
   tips?: string;
   best_time?: string;
-  weather_spring?: string;
-  weather_summer?: string;
-  weather_autumn?: string;
-  weather_winter?: string;
   avg_score?: number; // 리뷰 평균 점수 (0-5)
   review_count?: number;  // 리뷰 개수
+  // weather_spring?: string;
+  // weather_summer?: string;
+  // weather_autumn?: string;
+  // weather_winter?: string;
 }
 
 export interface FilterState {
@@ -25,4 +27,13 @@ export interface FilterState {
   selectedRegion: string;
   selectedBudget: string;
   showPopularOnly: boolean;
+}
+//홈화면용
+export interface PopularDestination extends Omit<Destination, 'rating_num' | 'view_count'> { // rating_num, view_count는 필요없으면 omit
+  avg_score: number;
+  review_count: number;
+}
+export interface DetailedDestination extends Destination {
+  avg_score?: number; // 목록에서는 이 값이 있을 수도 있고 없을 수도 있음
+  review_count?: number; // 목록에서는 이 값이 있을 수도 있고 없을 수도 있음
 }
