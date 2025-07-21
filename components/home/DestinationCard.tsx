3abc00d70dcb7294e2185f353aa1cc133bae37ab
Park-seungg <parkis1101@gmail.com>
2025-07-21 09:29:01 +0900

    최종

--- components/home/DestinationCard.tsx
+++ components/home/DestinationCard.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Destination } from '@/utils/destination/types';
 
+
 interface DestinationCardProps {
   destination: Destination;
 }
